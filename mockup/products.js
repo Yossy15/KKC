883e2fb62129c9f8b266fab890dc757643184e0c
Suwijak Jaisuk <gameimp72@gmail.com>
2026-09-13 15:23:58 +0700

    feat: add product filtering, detail views, and mock data

--- mockup/products.js
+++ mockup/products.js
@@ -10,13 +10,17 @@ const productTypes = [
 const sizes = ["S", "M", "L"];
 const prices = [20, 25, 30, 35];
 const colors = ["ดำ", "ขาว", "เทา", "น้ำเงิน", "แดง"];
+const status = ["พร้อมส่ง", "ขายแล้ว"];
+const payments = ["ชำระแล้ว", "รอชำระเงิน", "ยกเลิก"];
 
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 200; i++) {
   const productType = productTypes[i % productTypes.length];
 
   const randomSize = sizes[Math.floor(Math.random() * sizes.length)];
   const randomPrice = prices[Math.floor(Math.random() * prices.length)];
   const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  const randomStatus = status[Math.floor(Math.random() * status.length)];
+  const randomPayment = payments[Math.floor(Math.random() * payments.length)];
 
   mockProducts.push({
     id: i,
@@ -26,5 +30,7 @@ for (let i = 1; i <= 100; i++) {
     size: randomSize,
     price: randomPrice,
     color: randomColor,
+    status: randomStatus,
+    payments: randomPayment,
   });
 }
